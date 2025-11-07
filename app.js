9efe252df9a9692c08ea602d10a7afb4c707dab6
Ayubxon Xamidov <ayyuubxonxamidov@gmail.com>
2025-11-07 10:01:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Uyga ish
+
+//111
+let ar = [[1,2], [3,4], [5]];
+console.log(ar.flat());
+
+
+//112
+
+
+
+let users = [{name:"Ali",age:25},{name:"Vali",age:17},{name:"Sami",age:20},{name:"John",age:30}]
+users.forEach(user => {
+    if (user.age > 18 && user.name.toLowerCase().includes("a")) {
+      console.log(user);
+    }
+  });
+
+//113
